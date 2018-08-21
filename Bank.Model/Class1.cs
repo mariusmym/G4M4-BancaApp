44541b1776c1b2de9c0985184eaf67e4afd5bac8
Marius M <marius_mym@yahoo.com>
2018-08-21 20:03:22 +0300

    adaugare model

---
+++ Bank.Model/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Model
+{
+    public class Class1
+    {
+        public string getljkdD()
+        {
+            return "dkljh";
+        }
+    }
+
+    
+}
